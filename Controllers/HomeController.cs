ba991a5d382dd5cee981ad460b7d89215dfa392e
akram350 <akram.333.kjk@gmail.com>
2023-05-27 17:46:23 -0400

    adding home page view and top selling products

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using CoffeeShop.Models.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoffeeShop.Controllers
+{
+	
+	public class HomeController : Controller
+	{
+
+		private IProductRepository productRepository;
+		public HomeController(IProductRepository productRepository)
+		{
+			this.productRepository = productRepository;
+		}
+		public IActionResult Index()
+		{
+			var trendingProducts = productRepository.GetTrendingProducts();
+			return View(trendingProducts);
+		}
+	}
+}
