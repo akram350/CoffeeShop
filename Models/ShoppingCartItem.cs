dd045d875623e7109ae6da0749d4a8f6a8be78ae
akram350 <akram.333.kjk@gmail.com>
2023-05-31 00:40:44 -0400

    adding a partial View

---
+++ Models/ShoppingCartItem.cs
@@ -0,0 +1,11 @@
+﻿namespace CoffeeShop.Models
+{
+    public class ShoppingCartItem
+    {
+
+        public int Id { get; set; }
+        public int Qty { get; set; }
+        public Product Product { get; set; }
+        public int ShoppingCartId { get; set; }
+    }
+}
