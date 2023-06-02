1df94ffa8001b46269825b4d4fbde28d8ab2bc48
akram350 <akram.333.kjk@gmail.com>
2023-06-02 01:09:19 -0400

    ShoppingCart Services done

--- Models/ShoppingCartItem.cs
+++ Models/ShoppingCartItem.cs
@@ -5,7 +5,7 @@
 
         public int Id { get; set; }
         public int Qty { get; set; }
-        public Product Product { get; set; }
-        public int ShoppingCartId { get; set; }
+        public Product? Product { get; set; }
+        public string? ShoppingCartId { get; set; }
     }
 }
--- Program.cs
+++ Program.cs
@@ -8,10 +8,13 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 builder.Services.AddScoped<IProductRepository, ProductRepository>();
+builder.Services.AddScoped<IShoppingCartRepository, ShoppingCartRepository>(ShoppingCartRepository.GetCart);
+
 builder.Services.AddDbContext<CoffeeShopDbContext>(option=>option.UseSqlServer("Server=LAPTOP-VJVEJJS2\\SQLEXPRESS;Database=CoffeeShopDb;Trusted_Connection=True;TrustServerCertificate=true;MultipleActiveResultSets=true"));
-
+builder.Services.AddSession();
+builder.Services.AddHttpContextAccessor();
 var app = builder.Build();
-
+app.UseSession();
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
